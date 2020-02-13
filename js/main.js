19bb8f3bd0a172c89e6e9a486cdf7afaf9eb9262
Vlad <arnu90@gmail.com>
2020-02-13 23:27:25 +0200

    fix way2

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ labelDark.addEventListener('click',()=>{
     if(!checkboxDark.checked){
         darkcss = document.createElement('link');
         darkcss.rel = 'stylesheet';
-        darkcss.href = '/css/dark.css';
+        darkcss.href = 'css/dark.css';
         let  head = document.querySelector('head');
         head.append(darkcss);
     }
