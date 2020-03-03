25414f4c1d110fe3f970392d85a85d87b0d8df45
Vlad <arnu90@gmail.com>
2020-03-03 17:38:09 +0200

    fix scroll

--- js/main.js
+++ js/main.js
@@ -118,15 +118,70 @@ let html = document.querySelector('html');
 up.addEventListener('click',()=>{
     if(html.scrollTop>0)
     {
+        disableScroll();
          let up = setInterval(()=>{
             html.scrollTop -=30 ;
             if(html.scrollTop==0)
             {
+                enableScroll();
                 clearInterval(up);
             }
         },1);
     }
 })
+
+
+
+
+
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  document.addEventListener('wheel', preventDefault, {passive: false}); // Disable scrolling in Chrome
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    document.removeEventListener('wheel', preventDefault, {passive: false}); // Enable scrolling in Chrome
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //-----------white-dark----------------
 
